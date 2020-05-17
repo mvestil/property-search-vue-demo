052b25af15b4f3a791a58666f3651c5adfc68acf
Mark Bonnie Vestil <mbvestil@gmail.com>
2020-05-17 23:08:07 +1000

    optimzation

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -74,6 +74,7 @@
 
 <script>
 import "./sidebar.css";
+import { property } from "../../config";
 
 export default {
   name: "sidebar",
@@ -93,23 +94,8 @@ export default {
       },
 
       // predefined lists but can come from an API backend
-      states: [
-        { text: "Select One", value: null },
-        { text: "NSW", value: "New South Wales" },
-        { text: "VIC", value: "Victoria" },
-        { text: "QLD", value: "Queensland" },
-        { text: "NT", value: "Northern Territory" },
-        { text: "SA", value: "South Australia" },
-        { text: "TAS", value: "Tasmania" },
-        { text: "WA", value: "Western Australia" }
-      ],
-      stages: [
-        { text: "DA Approved", value: "da approved" },
-        { text: "DA Pending", value: "da pending" },
-        { text: "DA Refused", value: "da refused" },
-        { text: "Construction", value: "construction" },
-        { text: "Other", value: "other" }
-      ]
+      states: property.states,
+      stages: property.stages
     };
   },
 
---
+++ src/config/index.js
@@ -0,0 +1,20 @@
+export const property = {
+  states: [
+    { text: "Select One", value: null },
+    { text: "NSW", value: "New South Wales" },
+    { text: "VIC", value: "Victoria" },
+    { text: "QLD", value: "Queensland" },
+    { text: "NT", value: "Northern Territory" },
+    { text: "SA", value: "South Australia" },
+    { text: "TAS", value: "Tasmania" },
+    { text: "WA", value: "Western Australia" },
+  ],
+
+  stages: [
+    { text: "DA Approved", value: "da approved" },
+    { text: "DA Pending", value: "da pending" },
+    { text: "DA Refused", value: "da refused" },
+    { text: "Construction", value: "construction" },
+    { text: "Other", value: "other" },
+  ],
+};
